92d93a282af50a5ab00d31d3a9cf65521d8616af
EduardoPereira22 <107266971+EduardoPereira22@users.noreply.github.com>
2022-07-18 18:46:54 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,15 @@
+const botaocadastro = document.querySelector("#botaocadastro");
+
+botaocadastro.addEventListener("click", function(e){
+
+const nome = document.querySelector("#nome");
+
+
+const nomevalue = nome.value;
+
+console.log(nomevalue)
+
+
+
+
+})
